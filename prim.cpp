1de64cdcc48990460b1c9aee348ccdba603de706
zamee0 <zamee5721@gmail.com>
2026-07-04 20:31:06 +0600

    prim updated

--- prim.cpp
+++ prim.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node
+{
+public:
+    int w;
+    int parent;
+    int u;
+
+    bool operator>(const Node &other) const
+    {
+        return w > other.w;
+    }
+};
+
+class Edge
+{
+public:
+    int u,v,w;
+};
+
 int main()
 {
     int n,m;
@@ -17,43 +36,44 @@ int main()
         adj[v].push_back({u,w});
     }
 
-    priority_queue<
-    pair<int,int>,
-    vector<pair<int,int>>,
-    greater<pair<int,int>>> pq;
+    priority_queue<Node, vector<Node>, greater<Node>> pq;
 
     vector<bool> visited(n+1,false);
 
-    pq.push({0,1});
+    vector<Edge> mst;
+
+    pq.push({0,-1,1});      // weight,parent,node
 
     int cost=0;
     int taken=0;
 
     while(!pq.empty())
     {
-        auto cur=pq.top();
+        Node cur=pq.top();
         pq.pop();
 
-        int weight=cur.first;
-        int node=cur.second;
-
-        if(visited[node])
+        if(visited[cur.u])
             continue;
 
-        visited[node]=true;
+        visited[cur.u]=true;
 
-        cost+=weight;
-
+        cost+=cur.w;
         taken++;
 
-        for(auto child:adj[node])
+    
+        if(cur.parent!=-1)
+        {
+            mst.push_back({cur.parent,cur.u,cur.w});
+        }
+
+        for(auto child:adj[cur.u])
         {
             int next=child.first;
             int w=child.second;
 
             if(!visited[next])
             {
-                pq.push({w,next});
+                pq.push({w,cur.u,next});
             }
         }
     }
@@ -64,5 +84,14 @@ int main()
         return 0;
     }
 
-    cout<<cost;
+    cout<<"Minimum Cost = "<<cost<<endl;
+
+    cout<<"MST Edges"<<endl;
+
+    for(auto e:mst)
+    {
+        cout<<e.u<<" "<<e.v<<" "<<e.w<<endl;
+    }
+
+    return 0;
 }
